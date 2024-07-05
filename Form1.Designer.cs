bcdf816aa7bf7ab0f18816506be0da3405332f93
k.mucha2 <k.mucha2@samsung.com>
2024-07-05 12:11:57 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace Roulette_Konrad_Mucha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_plus_001 = new System.Windows.Forms.Button();
+            this.button_plus_01 = new System.Windows.Forms.Button();
+            this.button_plus_1 = new System.Windows.Forms.Button();
+            this.button_plus_10 = new System.Windows.Forms.Button();
+            this.button_plus_100 = new System.Windows.Forms.Button();
+            this.button_place_bid = new System.Windows.Forms.Button();
+            this.button_plus_max = new System.Windows.Forms.Button();
+            this.bid_value_text_box = new System.Windows.Forms.RichTextBox();
+            this.button_minus_001 = new System.Windows.Forms.Button();
+            this.button_minus_01 = new System.Windows.Forms.Button();
+            this.button_minus_1 = new System.Windows.Forms.Button();
+            this.button_minus_10 = new System.Windows.Forms.Button();
+            this.button_minus_100 = new System.Windows.Forms.Button();
+            this.button_minus_max = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // button_plus_001
+            // 
+            this.button_plus_001.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button_plus_001.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button_plus_001.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_plus_001.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_plus_001.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold);
+            this.button_plus_001.ForeColor = System.Drawing.Color.DimGray;
+            this.button_plus_001.Location = new System.Drawing.Point(202, 176);
+            this.button_plus_001.Name = "button_plus_001";
+            this.button_plus_001.Size = new System.Drawing.Size(92, 39);
+            this.button_plus_001.TabIndex = 0;
+            this.button_plus_001.Text = "+ 0,01";
+            this.button_plus_001.UseVisualStyleBackColor = false;
+            this.button_plus_001.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button_plus_01
+            // 
+            this.button_plus_01.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button_plus_01.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button_plus_01.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_plus_01.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_plus_01.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold);
+            this.button_plus_01.ForeColor = System.Drawing.Color.DimGray;
+            this.button_plus_01.Location = new System.Drawing.Point(300, 176);
+            this.button_plus_01.Name = "button_plus_01";
+            this.button_plus_01.Size = new System.Drawing.Size(92, 39);
+            this.button_plus_01.TabIndex = 1;
+            this.button_plus_01.Text = "+ 0,1";
+            this.button_plus_01.UseVisualStyleBackColor = false;
+            // 
+            // button_plus_1
+            // 
+            this.button_plus_1.BackColor = System.Drawing.Color.Red;
+            this.button_plus_1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button_plus_1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_plus_1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_plus_1.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold);
+            this.button_plus_1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.button_plus_1.Location = new System.Drawing.Point(399, 176);
+            this.button_plus_1.Name = "button_plus_1";
+            this.button_plus_1.Size = new System.Drawing.Size(92, 39);
+            this.button_plus_1.TabIndex = 2;
+            this.button_plus_1.Text = "+ 1";
+            this.button_plus_1.UseVisualStyleBackColor = false;
+            // 
+            // button_plus_10
+            // 
+            this.button_plus_10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button_plus_10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button_plus_10.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_plus_10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_plus_10.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold);
+            this.button_plus_10.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.button_plus_10.Location = new System.Drawing.Point(497, 176);
+            this.button_plus_10.Name = "button_plus_10";
+            this.button_plus_10.Size = new System.Drawing.Size(92, 39);
+            this.button_plus_10.TabIndex = 3;
+            this.button_plus_10.Text = "+ 10";
+            this.button_plus_10.UseVisualStyleBackColor = false;
+            // 
+            // button_plus_100
+            // 
+            this.button_plus_100.BackColor = System.Drawing.Color.Maroon;
+            this.button_plus_100.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button_plus_100.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_plus_100.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_plus_100.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold);
+            this.button_plus_100.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.button_plus_100.Location = new System.Drawing.Point(595, 176);
+            this.button_plus_100.Name = "button_plus_100";
+            this.button_plus_100.Size = new System.Drawing.Size(92, 39);
+            this.button_plus_100.TabIndex = 4;
+            this.button_plus_100.Text = "+ 100";
+            this.button_plus_100.UseVisualStyleBackColor = false;
+            // 
+            // button_place_bid
+            // 
+            this.button_place_bid.BackColor = System.Drawing.Color.LimeGreen;
+            this.button_place_bid.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold);
+            this.button_place_bid.Location = new System.Drawing.Point(832, 176);
+            this.button_place_bid.Name = "button_place_bid";
+            this.button_place_bid.Size = new System.Drawing.Size(198, 84);
+            this.button_place_bid.TabIndex = 5;
+            this.button_place_bid.Text = "PLACE BID";
+            this.button_place_bid.UseVisualStyleBackColor = false;
+            // 
+            // button_plus_max
+            // 
+            this.button_plus_max.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button_plus_max.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button_plus_max.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_plus_max.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_plus_max.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold);
+            this.button_plus_max.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.button_plus_max.Location = new System.Drawing.Point(693, 176);
+            this.button_plus_max.Name = "button_plus_max";
+            this.button_plus_max.Size = new System.Drawing.Size(92, 39);
+            this.button_plus_max.TabIndex = 6;
+            this.button_plus_max.Text = "+ MAX";
+            this.button_plus_max.UseVisualStyleBackColor = false;
+            // 
+            // bid_value_text_box
+            // 
+            this.bid_value_text_box.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold);
+            this.bid_value_text_box.Location = new System.Drawing.Point(33, 176);
+            this.bid_value_text_box.Name = "bid_value_text_box";
+            this.bid_value_text_box.Size = new System.Drawing.Size(129, 84);
+            this.bid_value_text_box.TabIndex = 7;
+            this.bid_value_text_box.Text = "";
+            this.bid_value_text_box.TextChanged += new System.EventHandler(this.bid_value_text_box_TextChanged);
+            // 
+            // button_minus_001
+            // 
+            this.button_minus_001.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button_minus_001.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button_minus_001.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_minus_001.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_minus_001.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_minus_001.ForeColor = System.Drawing.Color.DimGray;
+            this.button_minus_001.Location = new System.Drawing.Point(203, 221);
+            this.button_minus_001.Name = "button_minus_001";
+            this.button_minus_001.Size = new System.Drawing.Size(92, 39);
+            this.button_minus_001.TabIndex = 8;
+            this.button_minus_001.Text = "- 0,01";
+            this.button_minus_001.UseVisualStyleBackColor = false;
+            // 
+            // button_minus_01
+            // 
+            this.button_minus_01.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button_minus_01.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button_minus_01.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_minus_01.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_minus_01.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold);
+            this.button_minus_01.ForeColor = System.Drawing.Color.DimGray;
+            this.button_minus_01.Location = new System.Drawing.Point(301, 221);
+            this.button_minus_01.Name = "button_minus_01";
+            this.button_minus_01.Size = new System.Drawing.Size(92, 39);
+            this.button_minus_01.TabIndex = 9;
+            this.button_minus_01.Text = "- 0,1";
+            this.button_minus_01.UseVisualStyleBackColor = false;
+            // 
+            // button_minus_1
+            // 
+            this.button_minus_1.BackColor = System.Drawing.Color.Red;
+            this.button_minus_1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button_minus_1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_minus_1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_minus_1.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold);
+            this.button_minus_1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.button_minus_1.Location = new System.Drawing.Point(399, 221);
+            this.button_minus_1.Name = "button_minus_1";
+            this.button_minus_1.Size = new System.Drawing.Size(92, 39);
+            this.button_minus_1.TabIndex = 10;
+            this.button_minus_1.Text = "- 1";
+            this.button_minus_1.UseVisualStyleBackColor = false;
+            // 
+            // button_minus_10
+            // 
+            this.button_minus_10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button_minus_10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button_minus_10.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_minus_10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_minus_10.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold);
+            this.button_minus_10.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.button_minus_10.Location = new System.Drawing.Point(497, 221);
+            this.button_minus_10.Name = "button_minus_10";
+            this.button_minus_10.Size = new System.Drawing.Size(92, 39);
+            this.button_minus_10.TabIndex = 11;
+            this.button_minus_10.Text = "- 10";
+            this.button_minus_10.UseVisualStyleBackColor = false;
+            // 
+            // button_minus_100
+            // 
+            this.button_minus_100.BackColor = System.Drawing.Color.Maroon;
+            this.button_minus_100.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button_minus_100.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_minus_100.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_minus_100.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold);
+            this.button_minus_100.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.button_minus_100.Location = new System.Drawing.Point(595, 221);
+            this.button_minus_100.Name = "button_minus_100";
+            this.button_minus_100.Size = new System.Drawing.Size(92, 39);
+            this.button_minus_100.TabIndex = 12;
+            this.button_minus_100.Text = "- 100";
+            this.button_minus_100.UseVisualStyleBackColor = false;
+            // 
+            // button_minus_max
+            // 
+            this.button_minus_max.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button_minus_max.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button_minus_max.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_minus_max.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_minus_max.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold);
+            this.button_minus_max.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.button_minus_max.Location = new System.Drawing.Point(693, 221);
+            this.button_minus_max.Name = "button_minus_max";
+            this.button_minus_max.Size = new System.Drawing.Size(92, 39);
+            this.button_minus_max.TabIndex = 13;
+            this.button_minus_max.Text = "- MAX";
+            this.button_minus_max.UseVisualStyleBackColor = false;
+            // 
+            // listView1
+            // 
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(33, 318);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(359, 341);
+            this.listView1.TabIndex = 14;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(31)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1189, 697);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button_minus_max);
+            this.Controls.Add(this.button_minus_100);
+            this.Controls.Add(this.button_minus_10);
+            this.Controls.Add(this.button_minus_1);
+            this.Controls.Add(this.button_minus_01);
+            this.Controls.Add(this.button_minus_001);
+            this.Controls.Add(this.bid_value_text_box);
+            this.Controls.Add(this.button_plus_max);
+            this.Controls.Add(this.button_place_bid);
+            this.Controls.Add(this.button_plus_100);
+            this.Controls.Add(this.button_plus_10);
+            this.Controls.Add(this.button_plus_1);
+            this.Controls.Add(this.button_plus_01);
+            this.Controls.Add(this.button_plus_001);
+            this.Name = "Form1";
+            this.Text = "Roulette";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_plus_001;
+        private System.Windows.Forms.Button button_plus_01;
+        private System.Windows.Forms.Button button_plus_1;
+        private System.Windows.Forms.Button button_plus_10;
+        private System.Windows.Forms.Button button_plus_100;
+        private System.Windows.Forms.Button button_place_bid;
+        private System.Windows.Forms.Button button_plus_max;
+        private System.Windows.Forms.RichTextBox bid_value_text_box;
+        private System.Windows.Forms.Button button_minus_001;
+        private System.Windows.Forms.Button button_minus_01;
+        private System.Windows.Forms.Button button_minus_1;
+        private System.Windows.Forms.Button button_minus_10;
+        private System.Windows.Forms.Button button_minus_100;
+        private System.Windows.Forms.Button button_minus_max;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
